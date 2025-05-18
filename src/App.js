5948316c3a0f8e9815479f0a9c6369fb4eca0940
Phung052004 <quangcuonkk2@gmail.com>
2025-05-18 14:52:12 +0700

    ngay2

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
 
-  return (
-    <div>HELLO WORLD &amp;
-      <MyComponent>
-
-      </MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>La Quang Phung
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>La Quang Phung
+        </p>
+        <MyComponent>
+        </MyComponent>
+      </div>
+    );
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>La Quang Phung
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,18 @@
 // class component
 import React, { Component } from 'react';
 class MyComponent extends React.Component {
+    state = {
+        name: 'Eric',
+        age: 18,
+        address: 'Hanoi',
+    }
     //JSX 
     render() {
         return (
             <div> My First Component
-                {Math.random()}
+                <br></br>
+                My name is {this.state.name}
+                My age is {this.state.age}
             </div>
         )
     }
